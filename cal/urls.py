dc6574db4a5de1cb8fcc5e26ef9f2d3c704423b2
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2022-09-05 22:25:04 -0300

    Editar e mudanças no HTML/CSS

--- cal/urls.py
+++ cal/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.CalendarView.as_view(), name='calendar'),
     path('^event/new/$', views.event, name='event_new'),
 	path('event/info/<str:pk>/', views.info_event, name='event_info'),
+    path('event/info/<str:event_id>/edit', views.event, name='event_edit'),
     path('event/info/<str:pk>/delete/', views.delete_event, name="event_delete"),
 ]
